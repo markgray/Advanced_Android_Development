a1c11e5518bf3227f6397ad8bc6b6de12d65fdd2
markgray <mark.t.gray@gmail.com>
2016-03-23 06:04:06 -0400

    Even more morphing of NewTime to GregorianCalendar

--- app/src/main/java/com/example/android/sunshine/app/sync/NewTime.java
+++ app/src/main/java/com/example/android/sunshine/app/sync/NewTime.java
@@ -1,6 +1,7 @@
 package com.example.android.sunshine.app.sync;
 
 import android.text.format.Time;
+import android.util.Log;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -13,6 +14,8 @@ import java.util.GregorianCalendar;
  */
 public class NewTime {
 
+    private static final String TAG = "NewTime";
+
     public final long gmtoffGC;
     public Time time;
     public GregorianCalendar gc;
@@ -21,22 +24,46 @@ public class NewTime {
     public NewTime() {
         time = new Time();
         GregorianCalendar gc = new GregorianCalendar();
-        gmtoffGC = gc.get(gc.DST_OFFSET) + gc.get(Calendar.ZONE_OFFSET);
+        gmtoffGC = gc.get(Calendar.DST_OFFSET) + gc.get(Calendar.ZONE_OFFSET);
         gmtoff = time.gmtoff;
+        if (gmtoff != gmtoffGC) {
+            Log.i(TAG, "Error: gmtoff=" + gmtoff + " gmtoffGC=" + gmtoffGC);
+        } else {
+            Log.i(TAG, "gmtoff=" + gmtoff + " gmtoffGC=" + gmtoffGC);
+        }
     }
 
     public void setToNow() {
         gc.setTimeInMillis(System.currentTimeMillis());
         time.setToNow();
+        if (gc.getTimeInMillis() != time.toMillis(true)) {
+            Log.i(TAG, "Error: gc=" + gc.getTimeInMillis() + " time=" + time.toMillis(true));
+        } else {
+            Log.i(TAG, "gc=" + gc.getTimeInMillis() + " time=" + time.toMillis(true));
+        }
     }
 
 
     public void set(long millis) {
         gc.setTimeInMillis(millis);
         time.set(millis);
+        if (gc.getTimeInMillis() != time.toMillis(true)) {
+            Log.i(TAG, "Error: gc=" + gc.getTimeInMillis() + " time=" + time.toMillis(true));
+        } else {
+            Log.i(TAG, "gc=" + gc.getTimeInMillis() + " time=" + time.toMillis(true));
+        }
     }
 
     public static int getJulianDay(long millis, long gmtoff) {
+        GregorianCalendar tempGC = new GregorianCalendar();
+        tempGC.setTimeInMillis(millis);
+        tempGC.set(Calendar.ZONE_OFFSET, (int) gmtoff);
+        if (tempGC.get(Calendar.DAY_OF_YEAR) != Time.getJulianDay(millis, gmtoff)) {
+            Log.i(TAG, "Error: GregorianCalendar Julian=" + tempGC.getTimeInMillis() + "Time Julian=" + Time.getJulianDay(millis, gmtoff));
+        } else {
+            Log.i(TAG, "GregorianCalendar Julian=" + tempGC.getTimeInMillis() + "Time Julian=" + Time.getJulianDay(millis, gmtoff));
+        }
+
         return Time.getJulianDay(millis, gmtoff);
     }
 
